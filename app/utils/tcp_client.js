3fc05da33a28ae42ea5a35107cc20fa299130a62
translucentleaf <translucentleaf@protonmail.com>
2019-06-14 11:08:37 -0500

    Working example of extremely basic networking functionality

--- app/utils/tcp_client.js
+++ app/utils/tcp_client.js
@@ -1,15 +1,18 @@
 import net from 'net';
 
-// TODO: Figure out how to send data
+// TODO: Figure out how to send a heal command as a tablet
 const connectNow = () => {
   const client = new net.Socket();
 
   client.connect(10000, 'localhost', () => {
     console.log('Did connect');
+    client.write('tablet steve');
   });
 
-  client.on('data', () => {
-    console.log('I received data!');
+  client.on('data', data => {
+    console.log('I received: ' + data);
+    client.write('heal23');
+    client.destroy();
   });
 
   client.on('close', () => {
